6a3d7c807b5d3ec541f80a4c82a8e77e8db890ff
ARJAY <ahriez.ponce2005@gmail.com>
2026-08-02 07:04:52 +0800

    Add PersonIdentity class for identity management

--- Java/Lessons/OOP/OOP Challenge/Identity.java
+++ Java/Lessons/OOP/OOP Challenge/Identity.java
@@ -1,1 +1,37 @@
+class PersonIdentity {
 
+    String name, city, religion, history, familyName;
+    int age, yr;
+    double ft;
+
+    PersonIdentity(String name, String city, String religion, String history, String familyName, int age, double ft, int yr) {
+        this.name = name;
+        this.age = age;
+        this.familyName = familyName;
+        this.religion = religion;
+        this.city = city;
+        this.history = history;
+        this.ft = ft;
+        this.yr = yr;
+    }
+
+    void displayIdentity() {
+        System.out.println("===================================================");
+        System.out.println("");
+        System.out.println("Name         : " + name);
+        System.out.println("Family Name  : " + familyName);
+        System.out.println("Age          : " + age);
+        System.out.println("Religion     : " + religion);
+        System.out.println("Foot         : " + ft);
+        System.out.println("Year of Stay : " + yr);
+        System.out.println("History      : " + history);
+    }
+}
+
+public class Identity {
+
+    public static void main(String[] args) {
+        PersonIdentity p = new PersonIdentity("Ahriez B. Ponce Jr.","Imus","Roman chatolic","Warrior","Ponce",21,5.4,5);
+        p.displayIdentity();
+    }
+}

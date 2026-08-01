732ebec33bf7db535f1f6ff130be7f49f41338c0
ARJAY <ahriez.ponce2005@gmail.com>
2026-08-01 12:18:35 +0800

    Implement Person class with encapsulation
    Added a Person class with encapsulation for name and age attributes, including getter and setter methods.

--- Java/Lessons/OOP/Encapsulation/Encapsulation.java
+++ Java/Lessons/OOP/Encapsulation/Encapsulation.java
@@ -1,1 +1,50 @@
+class Person {
+
+    private String name;
+    private int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+}
+
+public class Encapsulation {
+
+    public static void main(String[] args) {
+        Person p = new Person("Reddcode", 20);
+
+        String name1 = p.getName();
+        int age1 = p.getAge();
+
+        System.out.println("Name : " + name1);
+        System.out.println("Age  : " + age1);
+
+        p.setName("WhiteDCode");
+        p.setAge(21);
+
+        String name2 = p.getName();
+        int age2 = p.getAge();
+
+        System.out.println("Name : " + name2);
+        System.out.println("Age  : " + age2);
+    }
+}
 
